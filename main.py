ee5e08942b9a19d681f948629b3820a529556f67
Constantine Theocharis <kontheocharis@gmail.com>
2019-07-24 23:13:57 +0200

    New recordings, using FFT

--- main.py
+++ main.py
@@ -5,44 +5,54 @@ import numpy as np
 from typing import List
 import warnings
 
-filenames = [
-    ['recordings/10c-1.wav', 'recordings/10c-2.wav', 'recordings/10c-3.wav', 'recordings/10c-4.wav', 'recordings/10c-5.wav'],
-    ['recordings/20c-1.wav', 'recordings/20c-2.wav', 'recordings/20c-3.wav', 'recordings/20c-4.wav', 'recordings/20c-5.wav'],
-    ['recordings/50c-1.wav', 'recordings/50c-2.wav', 'recordings/50c-3.wav', 'recordings/50c-4.wav', 'recordings/50c-5.wav'],
-    ['recordings/R1-1.wav',  'recordings/R1-2.wav',  'recordings/R1-3.wav',  'recordings/R1-4.wav',  'recordings/R1-5.wav'],
-    ['recordings/R2-1.wav',  'recordings/R2-2.wav',  'recordings/R2-3.wav',  'recordings/R2-4.wav',  'recordings/R2-5.wav'],
-    ['recordings/R5-1.wav',  'recordings/R5-2.wav',  'recordings/R5-3.wav',  'recordings/R5-4.wav',  'recordings/R5-5.wav']]
+# filenames = [
+#     ['recordings/10c-1.wav', 'recordings/10c-2.wav', 'recordings/10c-3.wav', 'recordings/10c-4.wav', 'recordings/10c-5.wav'],
+#     ['recordings/20c-1.wav', 'recordings/20c-2.wav', 'recordings/20c-3.wav', 'recordings/20c-4.wav', 'recordings/20c-5.wav'],
+#     ['recordings/50c-1.wav', 'recordings/50c-2.wav', 'recordings/50c-3.wav', 'recordings/50c-4.wav', 'recordings/50c-5.wav'],
+#     ['recordings/R1-1.wav',  'recordings/R1-2.wav',  'recordings/R1-3.wav',  'recordings/R1-4.wav',  'recordings/R1-5.wav'],
+#     ['recordings/R2-1.wav',  'recordings/R2-2.wav',  'recordings/R2-3.wav',  'recordings/R2-4.wav',  'recordings/R2-5.wav'],
+#     ['recordings/R5-1.wav',  'recordings/R5-2.wav',  'recordings/R5-3.wav',  'recordings/R5-4.wav',  'recordings/R5-5.wav']]
 
-no_of_trials = 5
-time_buffer = 6000
-sampling_frequency = 0
+filenames = [
+    'recordings/10cent-eur.wav',
+    'recordings/1eur.wav',
+    'recordings/1rand.wav',
+    'recordings/20cent-eur.wav',
+    'recordings/20cent-rand.wav',
+    'recordings/2cent-eur.wav',
+    'recordings/2eur.wav',
+    'recordings/2rand.wav',
+    'recordings/50cent-eur.wav',
+    'recordings/50cent-rand.wav',
+    'recordings/5cent-eur.wav',
+    'recordings/5rand.wav']
+
+# no_of_trials = 5
+# time_buffer = 6000
+
+frequency_window = slice(500, 1025)
 
 
 def main():
-    powers = []
-    for i, filenames_for_coin in enumerate(filenames):
-        coin_name = filenames_for_coin[0].split('/')[1].split('-')[0]
+    data_to_plot = []
+    for j, filename in enumerate(filenames):
+        coin_name = filename.split('/')[1].split('.')[0]
 
-        spectral_densities = []
-        powers.append([])
-        for j, filename in enumerate(filenames_for_coin):
+        sampling_frequency, signal_data = get_data_from_file(filename)
+        frequency, time, spectrogram = signal.spectrogram(
+            signal_data, sampling_frequency, window=np.blackman(2048), nfft=2048)
 
-            sampling_frequency, signal_data = get_data_from_file(filename)
-            freq, spectral_density = data_to_spectral_density(sampling_frequency, signal_data)
+        fundemental_frequency = frequency[frequency_window][
+            np.argmax(np.sum(10 * np.log10(spectrogram[frequency_window]), axis=1))]
+        data_to_plot.append((coin_name, fundemental_frequency))
 
-            spectral_densities.append((freq, spectral_density))
+    zipped_data = list(zip(*data_to_plot))
+    labels = zipped_data[1]
 
-            powers[i].append(spectral_density_to_power(freq, spectral_density))
+    plot.bar(x=range(len(zipped_data[0])), height=zipped_data[1], tick_label=zipped_data[0])
 
-        freq, spectral_density = np.average(spectral_densities, axis=0)
-
-        # plot_spectral_density(coin_name, freq, spectral_density)
-        print_spectral_density(coin_name, freq, spectral_density)
-
-    plot.clf()
-    for power in zip(*powers):
-        print(power)
-        plot.plot(power)
+    for i, v in enumerate(labels):
+        plot.text(i-.25, v/labels[i]+100, labels[i], color='white')
     plot.show()
 
 
@@ -52,35 +62,11 @@ def get_data_from_file(filename: str) -> (int, List):
         warnings.simplefilter('ignore', wavfile.WavFileWarning)
         sampling_frequency, signal_data_full = wavfile.read(filename)
 
-    loudest_point = np.argmax(signal_data_full)
-
-    signal_data = signal_data_full[loudest_point - time_buffer:loudest_point + time_buffer]
+    # loudest_point = np.argmax(signal_data_full)
+    # signal_data = signal_data_full[loudest_point - time_buffer:loudest_point + time_buffer]
+    signal_data = signal_data_full
 
     return sampling_frequency, signal_data
-
-
-def data_to_spectral_density(sampling_frequency: int, signal_data: List) -> (List, List):
-    freq, spectral_density = signal.welch(signal_data, sampling_frequency)
-    return freq, spectral_density
-
-
-def spectral_density_to_power(freq: List, spectral_density: List) -> int:
-    return np.trapz(spectral_density, freq)
-
-
-def plot_spectral_density(name: str, freq: List, spectral_density: List):
-    fig, ax = plot.subplots()
-    ax.plot(freq, spectral_density)
-    ax.set_title(name + ' Coin Spectral Density')
-    ax.set_xlabel('Frequency')
-    ax.set_ylabel('Spectral Density')
-    fig.savefig('figures/' + name + '.png')
-
-
-def print_spectral_density(name: str, freq, spectral_density: List):
-    print('Highest frequency for ' + name + ' coin:')
-    print(freq[np.argmax(spectral_density)])
-    print(spectral_density_to_power(freq, spectral_density))
 
 
 if __name__ == "__main__":
